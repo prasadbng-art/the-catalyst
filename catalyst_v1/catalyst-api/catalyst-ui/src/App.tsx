817147d0e7fbe632ec7959213c7f766bd64fa973
Prasad-Kaiser <prasad.rao@outlook.com>
2026-01-21 12:02:37 +0530

    na

--- catalyst_v1/catalyst-api/catalyst-ui/src/App.tsx
+++ catalyst_v1/catalyst-api/catalyst-ui/src/App.tsx
@@ -1,22 +1,21 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AppShell from "./components/layout/AppShell";
 
 import BaselinePage from "./pages/baseline";
 import PersonaPage from "./pages/persona";
-
 import SimulationPage from "./pages/simulate";
-
-// inside <Routes>
-<Route path="/simulation" element={<SimulationPage />} />
 
 export default function App() {
   return (
     <BrowserRouter>
       <AppShell>
         <Routes>
-          <Route path="/" element={<BaselinePage />} />
+          {/* Default route */}
+          <Route path="/" element={<Navigate to="/baseline" replace />} />
+
           <Route path="/baseline" element={<BaselinePage />} />
           <Route path="/persona" element={<PersonaPage />} />
+          <Route path="/simulation" element={<SimulationPage />} />
         </Routes>
       </AppShell>
     </BrowserRouter>
