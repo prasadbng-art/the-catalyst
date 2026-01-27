b0180b6a88c5ef62b630d38bfe8373fa14e48728
Prasad-Kaiser <prasad.rao@outlook.com>
2026-01-27 14:22:14 +0530

    na

--- catalyst_v1/catalyst-api/catalyst-ui/src/App.tsx
+++ catalyst_v1/catalyst-api/catalyst-ui/src/App.tsx
@@ -1,20 +1,14 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
-import AppShell from "./components/layout/AppShell";
-import BaselinePage from "./pages/baseline";
-import Simulate from "./pages/simulate";
-
 export default function App() {
   return (
-    <BrowserRouter>
-      <AppShell>
-        <Routes>
-          <Route path="/" element={<Navigate to="/baseline" replace />} />
-          <Route path="/baseline" element={<BaselinePage />} />
-          {/* Route path="/simulate" element={<Simulate />} /> */}
-
-        </Routes>
-      </AppShell>
-    </BrowserRouter>
+    <div
+      style={{
+        padding: 40,
+        color: "white",
+        background: "black",
+        fontSize: 24,
+      }}
+    >
+      Catalyst is alive.
+    </div>
   );
 }
