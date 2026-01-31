c2d4a7b95d18b6f8749bea82cd111cc24a410dc0
Prasad-Kaiser <prasad.rao@outlook.com>
2026-01-31 14:59:54 +0530

    na

--- catalyst_v1/catalyst-api/catalyst-ui/src/components/layout/AppShell.tsx
+++ catalyst_v1/catalyst-api/catalyst-ui/src/components/layout/AppShell.tsx
@@ -3,9 +3,17 @@ import Sidebar from "./Sidebar";
 
 export default function AppShell() {
   return (
-    <div style={{ display: "flex", height: "100vh" }}>
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        border: "1px solid #0f172a", // visual reference frame
+        boxSizing: "border-box",
+        overflow: "hidden",
+      }}
+    >
       <Sidebar />
-      <main style={{ flex: 1, padding: 24 }}>
+      <main style={{ flex: 1 }}>
         <Outlet />
       </main>
     </div>
