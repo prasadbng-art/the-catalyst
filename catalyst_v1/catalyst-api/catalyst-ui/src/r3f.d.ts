9de2b9ee9287b5b5eb8731ef37e8cb68d64cc8cc
Prasad-Kaiser <prasad.rao@outlook.com>
2026-02-23 17:38:01 +0530

    react three augents

--- catalyst_v1/catalyst-api/catalyst-ui/src/r3f.d.ts
+++ catalyst_v1/catalyst-api/catalyst-ui/src/r3f.d.ts
@@ -1,1 +1,9 @@
-/// <reference types="@react-three/fiber" />
+import type { ThreeElements } from '@react-three/fiber'
+
+declare global {
+    namespace JSX {
+        interface IntrinsicElements extends ThreeElements { }
+    }
+}
+
+export { }
